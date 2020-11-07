2a0a5581940c9b7d7bc9812fd37252e1647c03c1
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:56:45 -0800

    extra: Disable RescueParty
    RescueParty's aggressive approach to error recovery almost never helps
    with the problems we're likely to face, and its risk of data loss is far
    too high. Force-disable it entirely and remove all other conditions,
    especially those that depend on data integrity in the first place, to
    reduce the risk of RescueParty triggering falsely and causing data loss.
    
    Note that it is still possible to force-enable RescueParty for testing
    purposes using the persist.sys.enable_rescue property.
    
    Change-Id: I21a86dab3a9693272a787f58a561fdc76131dc62

--- product.mk
+++ product.mk
@@ -3,6 +3,10 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+
 # Enable gestural navigation overlay to match default navigation mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
