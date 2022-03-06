fed9cfd567e56eb99fdcf60db69ef4d1d110b2f9
TheMalachite <eliasgheeraert@gmail.com>
2022-03-06 16:53:52 +0100

    extra: packages: Build ThemePicker
    Change-Id: I24aa9dededf1a2038d4d4d7454c18fc23b5803a2

--- product.mk
+++ product.mk
@@ -1,6 +1,10 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Packages
+PRODUCT_PACKAGES += \
+    ThemePicker
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/permissions/privapp_whitelist_com.android.wallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_com.android.wallpaper.xml
